ae6cec62dfea3572932542d4b42ed3663a1ad8ba
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-09-21 10:07:15 +0100

    .

---
+++ CLIENT/JS/index.js
@@ -0,0 +1,24 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBNjmQnhNhtKS3Fl-S01bPLapvqtjFN7kE",
+    authDomain: "myall-proekt.firebaseapp.com",
+    databaseURL: "https://myall-proekt.firebaseio.com",
+    projectId: "myall-proekt",
+    storageBucket: "myall-proekt.appspot.com",
+    messagingSenderId: "53126128195",
+    appId: "1:53126128195:web:c5605a60494f511b8b6af6",
+    measurementId: "G-2YRGN7EEZN"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  
+
+
+
+
+
+function CEKuser(a){
+	
+}
