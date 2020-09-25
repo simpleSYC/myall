546cc5ee1b2f6cc68d5b61cb8f411be7f6ce3866
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-09-25 11:37:57 +0100

    Update definiton.js

--- def/definiton.js
+++ def/definiton.js
@@ -3,22 +3,22 @@
 var A0=document.getElementsByClassName("general-button")[0];
 var A1=document.getElementsByClassName("writing-button")[0];
 var A2=document.getElementsByClassName("reading-button")[0];
-var arA=[[false],A0],[false],A1],[false],A2]];
+var arA=[[false],A0],[[false],A1],[[false],A2]];
 
 var S0=document.getElementsByClassName("testimonials-button")[0];
 var S1=document.getElementsByClassName("portfolio-button")[0];
 var S2=document.getElementsByClassName("contact-button")[0];
-var arS=[[false],S0],[false],S1],[false],S2]];
+var arS=[[false],S0],[[false],S1],[[false],S2]];
 
 var P0=document.getElementsByClassName("new-button")[0];
 var P1=document.getElementsByClassName("writing-button")[0];
 var P2=document.getElementsByClassName("bin-button")[0];
-var arP=[[false],P0],[false],P1],[false],P2]];
+var arP=[[false],P0],[[false],P1],[[false],P2]];
 
 var C0= document.getElementsByClassName("contact1-button")[0];
 var C1= document.getElementsByClassName("security-button")[0];
 var C2= document.getElementsByClassName("profile-button")[0];
-var arC=[[false],C0],[false],C1],[false],C2]];
+var arC=[[false],C0],[[false],C1],[[false],C2]];
 
 var AR=[arA,arS,arP,arC];
 
