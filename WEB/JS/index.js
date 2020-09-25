206df4e4accc669247fd30b78ca90091422e5417
1_KingEr <53094736+1kinger@users.noreply.github.com>
2020-09-25 01:54:09 +0100

    Update index.js

--- WEB/JS/index.js
+++ WEB/JS/index.js
@@ -25,7 +25,7 @@ var IME=document.getElementById("demo22");
 
 var MyallBase = firebase.database().ref();
 
-var LERo={"Activity":{"L0":"","L1":"","L2":"","L3":"","L4":"","L5":""},"Contact":{"L0":"","L1":"","L2":"","L3":""},"PROFIL":{"FOTO":"","IME":"","RANK":0,"REG":{"ID":"","email":"","veri":false},"SETINGS":{"S0":"","S1":""}},"Peyment":{"L0":"","L1":"","L2":"","L3":""},"Social":{"L0":"pop","L1":"","L2":"","L3":""}}
+var LERo={"Activity":{"L0":"","L1":"","L2":"","L3":"","L4":"","L5":""},"Contact":{"L0":"","L1":"","L2":"","L3":""},"PROFIL":{"FOTO":"","IME":"","RANK":0,"REG":{"ID":"","email":"","veri":false},"SETINGS":{"S0":"","S1":""},"STATUS":true},"Peyment":{"L0":"","L1":"","L2":"","L3":""},"Social":{"L0":"pop","L1":"","L2":"","L3":""}}
 
 
 var USERdef;
@@ -289,7 +289,9 @@ for(i=0;i<4;i++){
 	for(q=0;q<3;q++){
 		AR[i][q][0]=GTD[proz1[i].toString()][proz2[q].toString()];
 	}
-}GTD=null;	
+}
+GETaccSTATUS(GTD["PROFIL"]["STATUS"]);
+GTD=null;	
 
 }
 
